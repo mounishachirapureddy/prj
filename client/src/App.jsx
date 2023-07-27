06acacf29c015a8f6b018ce534af6a86b2250751
SasiVardhan247 <sasivardhan247@gmail.com>
2023-07-27 20:34:29 +0530

    service mesh config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,6 +50,17 @@ function App() {
         <Route path="/gaming-vendor-settings" element={<Settings />} />
         <Route path="/gaming-vendor-register" element={<Register />} />
 
+        {/*
+        <Route
+          path="/merchant-dashboard"
+          element={
+            merchantState.isLoggedIn ? (
+              <MerchantHome />
+            ) : (
+              <Navigate to="/merchant-login" />
+            )
+          }
+        /> */}
         <Route
           path="/merchant-profile"
           element={
@@ -73,11 +84,7 @@ function App() {
         <Route
           path="/merchant-dashboard"
           element={
-            merchantState.isLoggedIn ? (
               <MerchantHome />
-            ) : (
-              <Navigate to="/merchant-login" />
-            )
           }
         />
         <Route path="/merchant-signup" element={<MerchantSignup />} />
