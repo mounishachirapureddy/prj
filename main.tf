0a91662cc2d0f6cb881aa4857e3d780bbf991b16
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-11-28 12:32:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -289,3 +289,180 @@ resource "aws_security_group_rule" "control_plane_outbound" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 }
+resource "aws_eks_cluster" "main" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+    subnet_ids = var.eks_cluster_subnet_ids
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_eks_cluster_policy
+  ]
+}
+resource "aws_security_group" "eks_cluster" {
+  name        = var.cluster_sg_name
+  description = "Cluster communication with worker nodes"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = var.cluster_sg_name
+  }
+}
+
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+# Nodes in private subnets
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = var.private_subnet_ids
+
+  ami_type       = var.ami_type
+  disk_size      = var.disk_size
+  instance_types = var.instance_types
+
+  scaling_config {
+    desired_size = var.pvt_desired_size
+    max_size     = var.pvt_max_size
+    min_size     = var.pvt_min_size
+  }
+
+  tags = {
+    Name = var.node_group_name
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.aws_eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_read_only,
+  ]
+}
+
+# Nodes in public subnet
+resource "aws_eks_node_group" "public" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.node_group_name}-public"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = var.public_subnet_ids
+
+  ami_type       = var.ami_type
+  disk_size      = var.disk_size
+  instance_types = var.instance_types
+
+  scaling_config {
+    desired_size = var.pblc_desired_size
+    max_size     = var.pblc_max_size
+    min_size     = var.pblc_min_size
+  }
+
+  tags = {
+    Name = "${var.node_group_name}-public"
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.aws_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.aws_eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_read_only,
+  ]
+}
+#https://docs.aws.amazon.com/eks/latest/userguide/worker_node_IAM_role.html
+
+resource "aws_iam_role" "eks_nodes" {
+  name                 = "${var.eks_cluster_name}-worker"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_workers.json
+}
+
+data "aws_iam_policy_document" "assume_workers" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "aws_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+resource "aws_security_group" "eks_nodes" {
+  name        = var.nodes_sg_name
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name                                        = var.nodes_sg_name
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "owned"
+  }
+}
+
+resource "aws_security_group_rule" "nodes" {
+  description              = "Allow nodes to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "nodes_inbound" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_cluster.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
