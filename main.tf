af7f4ba51ca5beb4ce579bbc4b53954e0a2ceb34
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-11-28 17:19:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -274,27 +274,4 @@ resource "aws_iam_role" "workernodes" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role    = aws_iam_role.workernodes.name
  }
-Step 7. The last bit of code is to create the worker nodes. For testing purposes, use just one worker node in the scaling_config configuration. In production, follow best practices and use at least three worker nodes.
 
- resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.devopsthehardway-eks.name
-  node_group_name = "devopsthehardway-workernodes"
-  node_role_arn  = aws_iam_role.workernodes.arn
-  subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
-  instance_types = ["t2.mediuml"]
- 
-  scaling_config {
-   desired_size = 1
-   max_size   = 1
-   min_size   = 1
-  }
- 
-  depends_on = [
-   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
- }
-
-  ]
- }
