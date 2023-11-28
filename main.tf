dc5db3d3109475ceefedd682550c3c5663bfff25
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-11-28 15:22:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,16 +14,14 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-# Create Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "my-igw"
-  }
-}
+
 
 # Create Subnets
 resource "aws_subnet" "subnet_a" {
+depends_on = [
+    aws_vpc.my-vpc
+  ]
+  
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
@@ -34,6 +32,9 @@ resource "aws_subnet" "subnet_a" {
 }
 
 resource "aws_subnet" "subnet_b" {
+depends_on = [
+    aws_vpc.my-vpc
+  ]
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
@@ -45,6 +46,10 @@ resource "aws_subnet" "subnet_b" {
 
 # Create Private Subnets
 resource "aws_subnet" "private_subnet_a" {
+depends_on = [
+    aws_vpc.my-vpc,
+    aws_subnet.subnet_a
+  ]
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-south-1a"
@@ -54,6 +59,10 @@ resource "aws_subnet" "private_subnet_a" {
 }
 
 resource "aws_subnet" "private_subnet_b" {
+depends_on = [
+    aws_vpc.my-vpc,
+    aws_subnet.subnet_b
+  ]
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "ap-south-1b"
@@ -61,5 +70,109 @@ resource "aws_subnet" "private_subnet_b" {
     Name = "private-subnet-b"
   }
 }
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+depends_on = [
+    aws_vpc.my-vpc,
+    aws_subnet.subnet_a,
+    aws_subnet.subnet_b
+  ]
+
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+}
+}
+# Creating an Route Table for the public subnet!
+resource "aws_route_table" "Public-Subnet-RT" {
+  depends_on = [
+    aws_vpc.my-vpc,
+    aws_internet_gateway.my_igw
+  ]
+
+   # VPC ID
+  vpc_id = aws_vpc.my-vpc.id
+
+  # NAT Rule
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "Route Table for Internet Gateway"
+  }
+}
+
+# Creating a resource for the Route Table Association!
+resource "aws_route_table_association" "RT-IG-Association" {
+
+  depends_on = [
+    aws_vpc.my-vpc,
+    aws_subnet.subnet_a,
+    aws_subnet.subnet_b,
+    aws_route_table.Public-Subnet-RT
+  ]
+
+# Public Subnet ID
+  subnet_id      = aws_subnet.subnet_a.id
+
+#  Route Table ID
+  route_table_id = aws_route_table.Public-Subnet-RT.id
+}
+# Creating an Elastic IP for the NAT Gateway!
+resource "aws_eip" "Nat-Gateway-EIP" {
+  depends_on = [
+    aws_route_table_association.RT-IG-Association
+  ]
+  vpc = true
+}
+# Creating a NAT Gateway!
+resource "aws_nat_gateway" "NAT_GATEWAY" {
+  depends_on = [
+    aws_eip.Nat-Gateway-EIP
+  ]
+
+  # Allocating the Elastic IP to the NAT Gateway!
+  allocation_id = aws_eip.Nat-Gateway-EIP.id
+  
+  # Associating it in the Public Subnet!
+  subnet_id = aws_subnet.subnet1.id
+  tags = {
+    Name = "Nat-Gateway_Project"
+  }
+}
+# Creating a Route Table for the Nat Gateway!
+resource "aws_route_table" "NAT-Gateway-RT" {
+  depends_on = [
+    aws_nat_gateway.NAT_GATEWAY
+  ]
+
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT_GATEWAY.id
+  }
+
+  tags = {
+    Name = "Route Table for NAT Gateway"
+  }
+
+}
+# Creating an Route Table Association of the NAT Gateway route 
+# table with the Private Subnet!
+resource "aws_route_table_association" "Nat-Gateway-RT-Association" {
+  depends_on = [
+    aws_route_table.NAT-Gateway-RT
+  ]
+
+#  Private Subnet ID for adding this route table to the DHCP server of Private subnet!
+  subnet_id      = aws_subnet.subnet_b.id
+
+# Route Table ID
+  route_table_id = aws_route_table.NAT-Gateway-RT.id
+}
+
 
 
