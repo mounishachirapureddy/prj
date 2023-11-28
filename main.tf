df29b37c2af1e4ff2c1eeeee6c3d265e9347ce0e
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-11-28 16:59:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -195,4 +195,82 @@ resource "aws_route_table_association" "Nat-Gateway-RT-Association" {
 }
 
 
+# Define IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com",
+      },
+    }],
+  })
+}
+
+# Attach AmazonEKSClusterPolicy to the IAM Role
+resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
+  name       = "eks-cluster-policy-attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  roles      = [aws_iam_role.eks_cluster_role.name]
+}
+
+# Define IAM Role for EKS Node Group
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com",
+      },
+    }],
+  })
+}
+
+# Attach policies to IAM Role for EKS Node Group
+resource "aws_iam_policy_attachment" "eks_node_group_policy_attachment" {
+  name       = "eks-node-group-policy-attachment"
+  roles      = [aws_iam_role.eks_node_group_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_policy_attachment" "eks_node_group_cni_attachment" {
+  name       = "eks-node-group-cni-policy-attachment"
+  roles      = [aws_iam_role.eks_node_group_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_policy_attachment" "eks_node_group_ecr_readonly_attachment" {
+  name       = "eks-node-group-ecr-readonly-policy-attachment"
+  roles      = [aws_iam_role.eks_node_group_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Create EKS Cluster
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.21"
+  subnets         = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+  vpc_id          = aws_vpc.my_vpc.id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      key_name = aws_key_pair.my_key_pair.key_name
+
+      # Add additional configurations as needed
+    }
+  }
+}
+
+
 
