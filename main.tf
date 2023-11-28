12121057531581f6fc1dc36a75b3f0052c659e89
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-11-28 11:54:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,17 +92,22 @@ resource "aws_nat_gateway" "nat_gateway_b" {
 
 # Create EKS Cluster
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  
   cluster_name    = "my-eks-cluster"
-  subnets         = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+  
   vpc_id          = aws_vpc.my_vpc.id
-  cluster_version = "1.21"
+  
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-    }
-  }
+  eks_managed_node_groups = {
+one = {
+    name = "node-group-1"
+
+    instance_types = ["t3.small"]
+
+    min_size     = 1
+    max_size     = 3
+    desired_size = 2
+}
+
+}
 }
