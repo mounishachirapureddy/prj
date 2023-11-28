cdd4227e5b27d51fc3f0e941c47ef7aa2e3afe6c
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-11-28 14:16:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,35 +96,9 @@ resource "aws_route" "main" {
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.main.id
 }
-# Create the private subnet
-resource "aws_subnet" "private_subnet" {
-  count = length(var.availability_zones)
-  vpc_id            = aws_vpc.custom_vpc.id
-  cidr_block = element(var.private_subnet_cidr_blocks, count.index)
-  availability_zone = element(var.availability_zones, count.index)
 
-  tags = {
-    Name = var.private_subnet_tag_name
-    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
 
-# Create the public subnet
-resource "aws_subnet" "public_subnet" {
-  count = length(var.availability_zones)
-  vpc_id            = "${aws_vpc.custom_vpc.id}"
-  cidr_block = element(var.public_subnet_cidr_blocks, count.index)
-  availability_zone = element(var.availability_zones, count.index)
 
-  tags = {
-    Name = "${var.public_subnet_tag_name}"
-    "kubernetes.io/cluster/${var.eks_cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = 1
-  }
-
-  map_public_ip_on_launch = true
-}
 
 # Create IGW for the public subnets
 resource "aws_internet_gateway" "igw" {
@@ -135,48 +109,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Route the public subnet traffic through the IGW
-resource "aws_route_table" "main" {
-  vpc_id = "${aws_vpc.custom_vpc.id}"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-
-  tags = {
-    Name = "${var.route_table_tag_name}"
-  }
-}
-
-# Route table and subnet associations
-resource "aws_route_table_association" "internet_access" {
-  count = length(var.availability_zones)
-  subnet_id      = "${aws_subnet.public_subnet[count.index].id}"
-  route_table_id = "${aws_route_table.main.id}"
-}
-
-# Create Elastic IP
-resource "aws_eip" "main" {
-  vpc              = true
-}
-
-# Create NAT Gateway
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.main.id
-  subnet_id     = aws_subnet.public_subnet[0].id
-
-  tags = {
-    Name = "NAT Gateway for Custom Kubernetes Cluster"
-  }
-}
-
-# Add route to route table
-resource "aws_route" "main" {
-  route_table_id            = aws_vpc.custom_vpc.default_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.main.id
-}
 # Security group for public subnet resources
 resource "aws_security_group" "public_sg" {
   name   = "public-sg"
