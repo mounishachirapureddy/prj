21525a8c8adbfb9bfe6d6f520853c104d2e5d667
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-12-19 12:26:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # Create VPC
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "${var.environment_name}_vpc" {
   cidr_block          = "10.0.0.0/16"
   enable_dns_support  = true
   enable_dns_hostnames = true
@@ -13,12 +13,12 @@ resource "aws_vpc" "my_vpc" {
 }
 
 # Create Subnets
-resource "aws_subnet" "subnet_a" {
+resource "aws_subnet" "${var.environment_name}_subnet_a" {
   depends_on = [
-    aws_vpc.my_vpc
+    aws_vpc.${var.environment_name}_vpc
   ]
   
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.${var.environment_name}_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
@@ -27,11 +27,11 @@ resource "aws_subnet" "subnet_a" {
   }
 }
 
-resource "aws_subnet" "subnet_b" {
+resource "aws_subnet" "${var.environment_name}_subnet_b" {
   depends_on = [
-    aws_vpc.my_vpc
+    aws_vpc.${var.environment_name}_vpc
   ]
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.${var.environment_name}_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
@@ -41,12 +41,12 @@ resource "aws_subnet" "subnet_b" {
 }
 
 # Create Private Subnets
-resource "aws_subnet" "private_subnet_a" {
+resource "aws_subnet" "${var.environment_name}_private_subnet_a" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a
+    aws_vpc.${var.environment_name}_vpc,
+    aws_subnet.${var.environment_name}_subnet_a
   ]
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.${var.environment_name}_vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-south-1a"
   tags = {
@@ -54,12 +54,12 @@ resource "aws_subnet" "private_subnet_a" {
   }
 }
 
-resource "aws_subnet" "private_subnet_b" {
+resource "aws_subnet" "${var.environment_name}_private_subnet_b" {
   depends_on = [
-    aws_vpc.my_vpc,
+    aws_vpc.${var.environment_name}_vpc,
     aws_subnet.subnet_b
   ]
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.${var.environment_name}_vpc.id
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "ap-south-1b"
   tags = {
@@ -68,33 +68,33 @@ resource "aws_subnet" "private_subnet_b" {
 }
 
 # Create Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
+resource "aws_internet_gateway" "${var.environment_name}_igw" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a,
-    aws_subnet.subnet_b
+    aws_vpc.${var.environment_name}_vpc,
+    aws_subnet.${var.environment_name}_subnet_a,
+    aws_subnet.${var.environment_name}_subnet_b
   ]
 
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.${var.environment_name}_vpc.id
   tags = {
     Name = "${var.environment_name}-igw"
   }
 }
 
 # Creating a Route Table for the public subnet
-resource "aws_route_table" "Public-Subnet-RT" {
+resource "aws_route_table" "${var.environment_name}_Public-Subnet-RT" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_internet_gateway.my_igw
+    aws_vpc.${var.environment_name}_vpc,
+    aws_internet_gateway.${var.environment_name}_igw
   ]
 
   # VPC ID
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.${var.environment_name}_vpc.id
 
   # NAT Rule
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.${var.environment_name}_igw.id
   }
 
   tags = {
@@ -103,39 +103,39 @@ resource "aws_route_table" "Public-Subnet-RT" {
 }
 
 # Creating a resource for the Route Table Association with subnet_a
-resource "aws_route_table_association" "RT-IG-Association-a" {
+resource "aws_route_table_association" "${var.environment_name}_RT-IG-Association-a" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a,
-    aws_subnet.subnet_b,
-    aws_route_table.Public-Subnet-RT
+    aws_vpc.${var.environment_name}_vpc,
+    aws_subnet.${var.environment_name}_subnet_a,
+    aws_subnet.${var.environment_name}_subnet_b,
+    aws_route_table.${var.environment_name}_Public-Subnet-RT
   ]
 
   # Public Subnet ID
-  subnet_id      = aws_subnet.subnet_a.id
+  subnet_id      = aws_subnet.${var.environment_name}_subnet_a.id
 
   #  Route Table ID
-  route_table_id = aws_route_table.Public-Subnet-RT.id
+  route_table_id = aws_route_table.${var.environment_name}_Public-Subnet-RT.id
 }
 
 # Creating a resource for the Route Table Association with subnet_b
-resource "aws_route_table_association" "RT-IG-Association-b" {
+resource "aws_route_table_association" "${var.environment_name}_RT-IG-Association-b" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_subnet.subnet_a,
-    aws_subnet.subnet_b,
-    aws_route_table.Public-Subnet-RT
+    aws_vpc.${var.environment_name}_vpc,
+    aws_subnet.${var.environment_name}_subnet_a,
+    aws_subnet.${var.environment_name}_subnet_b,
+    aws_route_table.${var.environment_name}_Public-Subnet-RT
   ]
 
   # Public Subnet ID
-  subnet_id      = aws_subnet.subnet_b.id
+  subnet_id      = aws_subnet.${var.environment_name}_subnet_b.id
 
   #  Route Table ID
-  route_table_id = aws_route_table.Public-Subnet-RT.id
+  route_table_id = aws_route_table.${var.environment_name}_Public-Subnet-RT.id
 }
 
 # Creating an Elastic IP for the NAT Gateway
-resource "aws_eip" "Nat-Gateway-EIP" {
+resource "aws_eip" "${var.environment_name}_Nat-Gateway-EIP" {
   vpc = true
   tags = {
     Name = "${var.environment_name}-nat-eip"
@@ -143,33 +143,33 @@ resource "aws_eip" "Nat-Gateway-EIP" {
 }
 
 # Creating a NAT Gateway
-resource "aws_nat_gateway" "NAT_GATEWAY" {
+resource "aws_nat_gateway" "${var.environment_name}_NAT_GATEWAY" {
   depends_on = [
-    aws_eip.Nat-Gateway-EIP,
-    aws_internet_gateway.my_igw
+    aws_eip.${var.environment_name}_Nat-Gateway-EIP,
+    aws_internet_gateway.${var.environment_name}_igw
   ]
 
   # Allocating the Elastic IP to the NAT Gateway
-  allocation_id = aws_eip.Nat-Gateway-EIP.id
+  allocation_id = aws_eip.${var.environment_name}_Nat-Gateway-EIP.id
 
   # Associating it in the Public Subnet
-  subnet_id = aws_subnet.subnet_a.id
+  subnet_id = aws_subnet.${var.environment_name}_subnet_a.id
   tags = {
     Name = "${var.environment_name}-Nat-Gateway_Project"
   }
 }
 
 # Creating a Route Table for the Nat Gateway
-resource "aws_route_table" "NAT-Gateway-RT" {
+resource "aws_route_table" "${var.environment_name}_NAT-Gateway-RT" {
   depends_on = [
-    aws_nat_gateway.NAT_GATEWAY
+    aws_nat_gateway.${var.environment_name}_NAT_GATEWAY
   ]
 
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.${var.environment_name}_vpc.id
 
   route {
     cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.NAT_GATEWAY.id
+    nat_gateway_id  = aws_nat_gateway.${var.environment_name}_NAT_GATEWAY.id
   }
 
   tags = {
@@ -178,17 +178,17 @@ resource "aws_route_table" "NAT-Gateway-RT" {
 }
 
 # Create a new Route Table for the Private Subnet
-resource "aws_route_table" "Private-Subnet-RT" {
+resource "aws_route_table" "${var.environment_name}_Private-Subnet-RT" {
   depends_on = [
-    aws_vpc.my_vpc,
-    aws_nat_gateway.NAT_GATEWAY
+    aws_vpc.${var.environment_name}_vpc,
+    aws_nat_gateway.${var.environment_name}NAT_GATEWAY
   ]
 
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.${var.environment_name}_vpc.id
 
   route {
     cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.NAT_GATEWAY.id
+    nat_gateway_id  = aws_nat_gateway.${var.environment_name}_NAT_GATEWAY.id
   }
 
   tags = {
@@ -198,28 +198,28 @@ resource "aws_route_table" "Private-Subnet-RT" {
 
 # Creating an Route Table Association of the NAT Gateway route 
 # table with the Private Subnet!
-resource "aws_route_table_association" "Private-Subnet-RT-Association-a" {
+resource "aws_route_table_association" "${var.environment_name}_Private-Subnet-RT-Association-a" {
   depends_on = [
-    aws_route_table.Private-Subnet-RT
+    aws_route_table.${var.environment_name}_Private-Subnet-RT
   ]
 
-  subnet_id      = aws_subnet.private_subnet_a.id
-  route_table_id = aws_route_table.Private-Subnet-RT.id
+  subnet_id      = aws_subnet.${var.environment_name}_private_subnet_a.id
+  route_table_id = aws_route_table.${var.environment_name}_Private-Subnet-RT.id
 }
 
 # Creating an Route Table Association of the NAT Gateway route 
 # table with the Private Subnet!
-resource "aws_route_table_association" "Private-Subnet-RT-Association-b" {
+resource "aws_route_table_association" "${var.environment_name}_Private-Subnet-RT-Association-b" {
   depends_on = [
-    aws_route_table.Private-Subnet-RT
+    aws_route_table.${var.environment_name}_Private-Subnet-RT
   ]
 
-  subnet_id      = aws_subnet.private_subnet_b.id
-  route_table_id = aws_route_table.Private-Subnet-RT.id
+  subnet_id      = aws_subnet.${var.environment_name}_private_subnet_b.id
+  route_table_id = aws_route_table.${var.environment_name}_Private-Subnet-RT.id
 }
 
 # Creating an AWS IAM role for the cluster
-resource "aws_iam_role" "eks-iam-role" {
+resource "aws_iam_role" "${var.environment_name}_eks-iam-role" {
   name = "${var.environment_name}-eks-iam-role"
 
   path = "/"
@@ -243,35 +243,35 @@ EOF
 # Attaching policies to the cluster
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-iam-role.name
+  role       = aws_iam_role.${var.environment_name}_eks-iam-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-iam-role.name
+  role       = aws_iam_role.${var.environment_name}_eks-iam-role.name
 }
 
 # Creating a cluster
 resource "aws_eks_cluster" "snappcoins-eks" {
   name     = "${var.environment_name}-snappcoins-cluster"
-  role_arn = aws_iam_role.eks-iam-role.arn
+  role_arn = aws_iam_role.${var.environment_name}_eks-iam-role.arn
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.subnet_a.id,
-      aws_subnet.subnet_b.id,
-      aws_subnet.private_subnet_a.id,
-      aws_subnet.private_subnet_b.id
+      aws_subnet.${var.environment_name}_subnet_a.id,
+      aws_subnet.${var.environment_name}_subnet_b.id,
+      aws_subnet.${var.environment_name}_private_subnet_a.id,
+      aws_subnet.${var.environment_name}_private_subnet_b.id
     ]
   }
 
   depends_on = [
-    aws_iam_role.eks-iam-role,
+    aws_iam_role.${var.environment_name}_eks-iam-role,
   ]
 }
 
 # Creating roles for nodes
-resource "aws_iam_role" "workernodes" {
+resource "aws_iam_role" "${var.environment_name}_workernodes" {
   name = "${var.environment_name}-eks-node-group"
 
   assume_role_policy = jsonencode({
@@ -289,34 +289,34 @@ resource "aws_iam_role" "workernodes" {
 # Attaching policies to nodes
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.workernodes.name
+  role       = aws_iam_role.${var.environment_name}_workernodes.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.workernodes.name
+  role       = aws_iam_role.${var.environment_name}_workernodes.name
 }
 
 resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role       = aws_iam_role.workernodes.name
+  role       = aws_iam_role.${var.environment_name}_workernodes.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.workernodes.name
+  role       = aws_iam_role.${var.environment_name}_workernodes.name
 }
 
 # Create a nodegroup
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "${var.environment_name}_example" {
   cluster_name    = aws_eks_cluster.snappcoins-eks.name
   node_group_name = "${var.environment_name}-node-group"
 
-  node_role_arn = aws_iam_role.workernodes.arn
+  node_role_arn = aws_iam_role.${var.environment_name}_workernodes.arn
 
   subnet_ids = [
-    aws_subnet.private_subnet_a.id,
-    aws_subnet.private_subnet_b.id
+    aws_subnet.${var.environment_name}_private_subnet_a.id,
+    aws_subnet.${var.environment_name}_private_subnet_b.id
   ]
 
   scaling_config {
