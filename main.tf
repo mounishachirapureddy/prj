9a87b2eb0ee9f28bdcb6d214af4a3b4246638b93
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-12-19 15:56:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,7 @@ resource "aws_vpc" "my_vpc" {
   cidr_block          = "10.0.0.0/16"
   enable_dns_support  = true
   enable_dns_hostnames = true
-  tags = {
-    Name = "${var.environment_name}-vpc"
-  }
+  
 }
 
 # Create Subnets
