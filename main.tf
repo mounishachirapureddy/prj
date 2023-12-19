d6b30bd2b871ef9f184f60f620c9715d1b56f92e
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-12-19 16:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ resource "aws_vpc" "my_vpc" {
   cidr_block          = "10.0.0.0/16"
   enable_dns_support  = true
   enable_dns_hostnames = true
+ tags = {
+    Name = "${var.environment_name}-vpc"
+  }
   
 }
 
