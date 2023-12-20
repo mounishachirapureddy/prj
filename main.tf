690e82ddbc915d1b91b9fc538f8517e5e3dc74f5
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2023-12-20 19:24:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ resource "aws_vpc" "my_vpc" {
   tags = {
     Name = "${var.environment_name}-vpc"
   }
- 
 }
 
 # Create Subnets
@@ -336,4 +335,21 @@ resource "aws_eks_node_group" "example" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_eks_cluster.snappcoins-eks
   ]
+}
+resource "null_resource" "install_istio" {
+  depends_on = [aws_eks_cluster.snappcoins-eks]
+
+  triggers = {
+    cluster_id = aws_eks_cluster.snappcoins-eks.id
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws eks update-kubeconfig --name dev-snappcoins-cluster --region ap-south-1
+      curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.1 TARGET_ARCH=x86_64 sh -
+      cd istio-1.20.1
+      export PATH=$PWD/bin:$PATH
+      istioctl install --set profile=demo -y
+    EOT
+  }
 }
