506e7f3ef6860c851613d68fc77fbbfee9cbfa58
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2024-01-05 11:10:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+terraform {
+  backend "s3" {
+    bucket = "terraform-snappcoins"
+    key    = "dev"
+    region = "ap-south-1"
+  }
+}
 # Create VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block          = "10.0.0.0/16"
